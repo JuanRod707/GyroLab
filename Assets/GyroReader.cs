5dc36c5f01c378cdfaef6465c5688249ea3124f0
Juancho <juan.rod707@gmail.com>
2019-03-02 12:32:34 -0300

    commit

---
+++ Assets/GyroReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroReader : MonoBehaviour
+{
+    public Text Attitude;
+    public Text Gravity;
+    public Text RotationRate;
+    public Text RotationRateUnbiased;
+    public Text UpdateInterval;
+    public Text UserAccel;
+
+    void Awake() => Input.gyro.enabled = true;
+
+    void Update()
+    {
+        var gyro = Input.gyro;
+
+        Attitude.text = gyro.attitude.ToString();
+        Gravity.text = gyro.gravity.ToString();
+        RotationRate.text = gyro.rotationRate.ToString();
+        RotationRateUnbiased.text = gyro.rotationRateUnbiased.ToString();
+        UpdateInterval.text = gyro.updateInterval.ToString();
+        UserAccel.text = gyro.userAcceleration.ToString();
+    }
+}
---
+++ Assets/RotateCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class RotateCube : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.rotation = Input.gyro.attitude;
+        }
+    }
+}
